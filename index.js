bda4bb063c97345b652af9101091ff354344d427
Armynerh <aimenah@outlook.com>
2023-07-17 20:09:01 +0100

    add index.js file

---
+++ index.js
@@ -0,0 +1,22 @@
+import { ManageBookDetails } from './modules/manageBook.js';
+import { updateDateTime } from './modules/dynamicDate.js';
+import { listToggle, addToggle, contactToggle } from './modules/booknav.js';
+
+const bookManager = new ManageBookDetails();
+window.onload = () => {
+  bookManager.loadBooks();
+  updateDateTime()
+  listToggle()
+  addToggle()
+  contactToggle()
+};
+
+const bookAdded = () => {
+  bookManager.bookAdded();
+};
+
+const btn = document.querySelector('.button');
+btn.addEventListener('click', () => {
+    bookAdded()
+  });
+
