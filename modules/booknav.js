093109585aa5011bb2f03a95c1b81ab99813c204
Armynerh <aimenah@outlook.com>
2023-07-17 22:03:36 +0100

    made changes based on reviews

--- modules/booknav.js
+++ modules/booknav.js
@@ -1,13 +1,13 @@
 const listnav = document.querySelector('.list-nav');
 const contactNav = document.querySelector('.contact-nav');
-const listContainer = document.querySelector('.container');
+const listContainer = document.querySelector('.container1');
 const addNav = document.querySelector('.add-nav');
 const form = document.querySelector('.form-cover');
 const contact = document.querySelector('.contact-container');
 
 export const listToggle = () => {
   listnav.addEventListener('click', () => {
-    listContainer.classList.add('active');
+    listContainer.classList.remove('active');
     form.classList.remove('active');
     contact.classList.remove('active');
   });
@@ -16,14 +16,14 @@ export const listToggle = () => {
 export const addToggle = () => {
   addNav.addEventListener('click', () => {
     form.classList.add('active');
-    listContainer.classList.remove('active');
+    listContainer.classList.add('active');
     contact.classList.remove('active');
   });
 };
 export const contactToggle = () => {
   contactNav.addEventListener('click', () => {
     contact.classList.add('active');
-    listContainer.classList.remove('active');
+    listContainer.classList.add('active');
     form.classList.remove('active');
   });
 };
--- modules/dynamicDate.js
+++ modules/dynamicDate.js
@@ -3,5 +3,5 @@ import { DateTime } from './luxon.min.js';
 export default function updateDateTime() {
   const dateTimeElement = document.getElementById('date-para');
   const newTime = DateTime.now();
-  dateTimeElement.textContent = newTime.toFormat('dd-MM-yyyy HH:mm:ss');
+  dateTimeElement.textContent = newTime.toFormat('FF');
 }
